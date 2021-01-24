495b39c99fb95ae8ed233b191afc7321326d0b1d
aschimmenti <andschimmenti@gmail.com>
2021-01-24 01:20:48 +0100

    added fade out p

--- article_selector.js
+++ article_selector.js
@@ -6,8 +6,11 @@ const capitalize = (s) => {
 function postArticle(articleName, issueJsonPath) { //try bastard function 
   $.ajax({url: articleName, 
     success: function(result){
+    $('#static').empty();
     $("#filerequest").empty();
-    $("#filerequest").html(result);
+    $("#filerequest").html(result).fadeIn("slow");
+    $("#metadata").empty();
+    $("#metadata").append('<h4>Metadata</h4><ul id="metadataList"></ul><h4>Metadata Issue</h4><div id="accordion"></div>')
     $("#metadataList").empty();
     var title = $("meta[name='DC.title']").attr('content');
     var creator = $("meta[name='DC.creator']").attr('content');
@@ -56,24 +59,6 @@ function postArticle(articleName, issueJsonPath) { //try bastard function
           counter = counter+1;
         });
       });
-
-      
-      /* <div class="card">
-    <div class="card-header" id="headingOne">
-      <h5 class="mb-0">
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </button>
-      </h5>
-    </div>
-          $('#accordion').append('');
-
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>*/ 
 }});};
 
 
